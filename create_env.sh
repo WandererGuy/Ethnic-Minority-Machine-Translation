0ac854b0a1679189e582613e4abbecd7f9f204c4
Duc Manh <manhtech264@gmail.com>
2025-02-28 21:50:04 +0700

    up

--- create_env.sh
+++ create_env.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 value=$(cat config/config_conda.txt)
-
-echo "Sourcing conda.sh to enable 'conda activate'..."
+echo "Sourcing /home/username/miniconda3/etc/profile.d/conda.sh to enable 'conda activate'..."
 source "$value"  # Adjust to your conda installation path
-
 current_dir=$(pwd)
 first_anaconda_env="$(pwd)/env_1"
+echo "$first_anaconda_env"
 second_anaconda_env="$(pwd)/env_2"
-
+echo "$second_anaconda_env"
 conda create -p $first_anaconda_env python=3.10 -y
 conda create -p $second_anaconda_env python=3.10 -y
 conda activate $first_anaconda_env
-pip install khmer-nltk
-pip install underthesea
-pip install nltk
-pip install numpy==1.25.0
+# pip install khmer-nltk
+# pip install underthesea
+# pip install nltk
+# pip install numpy==1.25.0
 pip install fastapi uvicorn pydantic python-multipart
 conda activate $second_anaconda_env
 cd OpenNMT-py
@@ -22,4 +21,3 @@ pip install -e .
 cd ..
 pip install fastapi uvicorn pydantic python-multipart
 pip install nltk
-
